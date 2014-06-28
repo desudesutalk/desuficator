55a805396754e975200adf157bbb48cbda7b8f91
desudesutalk <souseiseki.desu@yandex.ru>
2014-06-29 01:44:01 +0400

    Create desuficator.meta.js

---
+++ desuficator.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Desuficator for ORMT
+// @namespace    udp://desushelter/*
+// @version      0.0.1
+// @description  Desu-desu!
+// @include      http://dobrochan.com/b/res/*
+// @include      http://dobrochan.ru/b/res/*
+// @include      http://dobrochan.org/b/res/*
+// @copyright    2014+, Boku 
+// @updateURL    https://github.com/desudesutalk/desuficator/raw/master/desuficator.meta.js
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
